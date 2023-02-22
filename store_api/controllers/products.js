a89c20f4827584946f2b5ef37bb5268f3481ada6
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-02-22 12:44:57 +0800

    Add find name and basic sort

--- store_api/controllers/products.js
+++ store_api/controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res)=>{
-    // const products = await Product.find({})
-    res.status(200).json({msg: 'product route'})
+    const products = await Product.find({}).sort('name')
+    res.status(200).json({products, nbHist: products.length})
 }
 const getAllProducts = async(req, res)=>{
 
-    const{featured, company} = req.query;
+    const{featured, company, name} = req.query;
     const queryObject={};
 
     if(featured){
@@ -15,10 +15,13 @@ const getAllProducts = async(req, res)=>{
     if(company){
         queryObject.company = company;
     }
+    if(name){
+        queryObject.name = {$regex: name, $options:'i'};
+    }
     let result = Product.find(queryObject);
-
+    console.log(queryObject)
     const products = await result;
-    res.status(200).json({products});
+    res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {
