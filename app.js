48627848b6406fd2c304676230b9e92166f658c5
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-02-23 20:49:28 +0800

    Add fields

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+// require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+
+app.use(express.json())
+//routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+  });
+
+app.use('/api/v1/products', productsRouter);
+
+
+
+
+const port = process.env.PORT || 3000;
+
+const start = async()=>{
+    try {
+        //connect to Database
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=>{
+            console.log(`Server is listening to port ${port}....`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
