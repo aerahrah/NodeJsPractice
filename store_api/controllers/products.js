ea46262b2b9fef8502d0b0a5dd8474c8c044f83f
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-02-24 16:12:54 +0800

    Add skip

--- store_api/controllers/products.js
+++ store_api/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async(req, res)=>{
 }
 const getAllProducts = async(req, res)=>{
 
-    const{featured, company, name} = req.query;
+    const{featured, company, name, sort, fields} = req.query;
     const queryObject={};
 
     if(featured){
@@ -18,7 +18,24 @@ const getAllProducts = async(req, res)=>{
     if(name){
         queryObject.name = {$regex: name, $options:'i'};
     }
+
     let result = Product.find(queryObject);
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }else{
+        result = result.sort('createdAt');
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+      }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
     console.log(queryObject)
     const products = await result;
     res.status(200).json({products, nbHits: products.length});
