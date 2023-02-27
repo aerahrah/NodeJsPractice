ab060a2b66d2a7508ef2f065e687db799ab9c954
aerahrah <201910416@fit.edu.ph>
2023-02-27 21:01:39 +0800

    Implement database

---
+++ jwt_basic/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const connectDB = require('./db/connect')
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
