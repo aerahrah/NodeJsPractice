1ea97d34244ddb05c03cc2074208a290b8c364f9
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-05-22 22:38:38 +0800

    Added upload csv route

--- chatgptAi_nojeJs/chatgpt-example-main/index.js
+++ chatgptAi_nojeJs/chatgpt-example-main/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
-require("dotenv").config();
+const multer = require("multer");
+const csvParser = require("csv-parser");
+const fs = require("fs");
 const app = express();
 const port = 3500;
+
+require("dotenv").config();
+
+const upload = multer({ dest: "uploads/" });
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,19 +19,60 @@ const openai = new OpenAIApi(config);
 app.use(express.json());
 
 let conversation = [];
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const filePath = req.file.path;
+  let csvDataString = "";
+  const messages = [];
+
+  fs.createReadStream(filePath)
+    .pipe(csvParser())
+    .on("data", (data) => {
+      // console.log(data);
+      // console.log(data[0]);
+      const message = data.Name;
+      console.log(message);
+      if (message) {
+        csvDataString += message + "\n";
+      }
+    })
+    .on("end", async () => {
+      fs.unlinkSync(filePath);
+      messages.push({
+        role: "user",
+        content: csvDataString,
+      });
+      // const messagesJson = JSON.stringify(messages);
+
+      const response = await processMessages(messages);
+
+      res.json({ message: response });
+    });
+});
 
 app.post("/chat", async (req, res) => {
   const { message } = req.body;
+  console.log(message);
+  const messages = [
+    {
+      role: "system",
+      content: "You are a helpful assistant.",
+    },
+    {
+      role: "user",
+      content: message,
+    },
+  ];
 
-  conversation.push({
-    role: "system",
-    content: "You are a helpful assistant.",
-  });
-  conversation.push({
-    role: "user",
-    content: message,
-  });
+  const response = await processMessages(messages);
 
+  res.json({ message: response });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+async function processMessages(messages) {
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
@@ -34,29 +81,19 @@ app.post("/chat", async (req, res) => {
     method: "POST",
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: conversation,
+      messages: [...conversation, ...messages],
     }),
   });
-
   const responseData = await response.json();
-  const generatedMessage = responseData.choices[0].message.content;
-  conversation.push({
+  console.log(responseData);
+  const generatedMessages = responseData.choices.map((choice) => ({
     role: "assistant",
-    content: generatedMessage,
-  });
+    content: choice.message.content,
+  }));
 
-  res.json({ message: generatedMessage });
-});
+  // console.log(responseData);
+  // console.log(generatedMessages);
+  conversation.push(...messages, ...generatedMessages);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-//   const response = await openai.createCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages: conversation,
-//     max_tokens: 144,
-//     temperature: 0.7,
-//     n: 1,
-//     stop: "\n",
-//   });
+  return responseData;
+}
